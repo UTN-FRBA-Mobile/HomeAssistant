e82fe0d1d0701bc8b3d340c298955c8b74485c39
Alexis Herasimiuk <aherasimiuk@frba.utn.edu.ar>
2024-09-29 20:22:52 -0300

    #1 - Basic Scaffolding: TopBar, BottomBar

--- app/src/main/java/ar/edu/utn/frba/homeassistant/MainActivity.kt
+++ app/src/main/java/ar/edu/utn/frba/homeassistant/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ar.edu.utn.frba.homeassistant.ui.theme.HomeAssistantTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +29,38 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HomeAssistantTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeAssistantMainScaffold()
             }
         }
     }
 }
 
+// How to make a simple Scaffold: https://developer.android.com/develop/ui/compose/components/scaffold?hl=es-419
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun HomeAssistantMainScaffold() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Home Assistant") }
+            )
+        },
+        bottomBar = {
+            BottomAppBar {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    text = "Bottom app bar",
+                )
+            }
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HomeAssistantTheme {
-        Greeting("Android")
+        ) { }
+
     }
 }
