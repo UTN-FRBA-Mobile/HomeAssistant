8e04122a9cc285887141668f93731c915a89f4b8
Lucas Cabello <lucas_cabello@hotmail.com>
2024-11-24 17:19:03 -0300

    Added database update to clock automation

--- app/src/main/java/ar/edu/utn/frba/homeassistant/utils/receivers/AlarmReceiver.kt
+++ app/src/main/java/ar/edu/utn/frba/homeassistant/utils/receivers/AlarmReceiver.kt
@@ -5,14 +5,21 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 import ar.edu.utn.frba.homeassistant.GLOBAL_TAG
+import ar.edu.utn.frba.homeassistant.data.repository.AppRepository
 import ar.edu.utn.frba.homeassistant.network.UdpService
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 const val ALARM_RECEIVER_TAG = "$GLOBAL_TAG#ALARM_BROADCAST_RECEIVER"
 
+@AndroidEntryPoint
 class AlarmReceiver : BroadcastReceiver() {
+    @Inject
+    lateinit var appRepository: AppRepository
+
     override fun onReceive(context: Context?, intent: Intent?) {
         Log.d(ALARM_RECEIVER_TAG, "[onReceive]: Alarm broadcast received")
         if (context == null || intent == null){
@@ -28,9 +35,11 @@ class AlarmReceiver : BroadcastReceiver() {
                 if(shouldTurnOn){
                     Log.d(ALARM_RECEIVER_TAG, "[onReceive]: Sending UDP message to device $it - toggle:on")
                     UdpService.sendUdpMessage(it, "toggle:on")
+                    appRepository.updateIsOn(it, true)
                 } else {
                     Log.d(ALARM_RECEIVER_TAG, "[onReceive]: Sending UDP message to device $it - toggle:off")
                     UdpService.sendUdpMessage(it, "toggle:off")
+                    appRepository.updateIsOn(it, false)
                 }
             }
         }
